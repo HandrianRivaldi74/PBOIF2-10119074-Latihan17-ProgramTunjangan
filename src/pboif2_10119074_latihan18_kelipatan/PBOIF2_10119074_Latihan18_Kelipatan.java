6d7c626c187980f5c5a839095b2ced26727099e4
HandrianRivaldi74 <handrianr28@gmail.com>
2020-10-31 18:50:09 +0700

    Latihan18

---
+++ src/pboif2_10119074_latihan18_kelipatan/PBOIF2_10119074_Latihan18_Kelipatan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119074_latihan18_kelipatan;
+
+/**
+ *
+ * @author Acer
+ * NAMA     : Handrian Rivaldi
+ * KELAS    : PBOIF2
+ * NIM      : 10119074
+ * Deskripsi : Menampilkan Perulangan kelipatan
+ */
+public class PBOIF2_10119074_Latihan18_Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float i,j;
+        j =7/2;
+            for (i=j; i<=35; i+=j){
+                if(i%2==0)
+                        continue;
+            System.out.println (i);{
+            }
+        }
+    }
+    
+}
